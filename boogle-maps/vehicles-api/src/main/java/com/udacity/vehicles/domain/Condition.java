0da0142d165865352bbb51504ed8cfcb9034e8e9
Miroslav Maksimovic <miroslavmaksimovic95@gmail.com>
2023-06-28 18:33:43 +0200

    Add Car, CarRepository, Details, Location and Condition

---
+++ boogle-maps/vehicles-api/src/main/java/com/udacity/vehicles/domain/Condition.java
@@ -0,0 +1,12 @@
+package com.udacity.vehicles.domain;
+
+import io.swagger.annotations.ApiModel;
+
+/**
+ * Available values for condition of a given car.
+ */
+
+public enum Condition {
+    USED,
+    NEW
+}
